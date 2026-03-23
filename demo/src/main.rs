876bec7455b7d5b37ae7babb1ce5c688f0e57b38
Gabor Szabo <gabor@szabgab.com>
2026-03-23 17:49:26 +0100

    add changed

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    println!("{}", add(2, 3));
+    println!("{}", add('+', 2, 3));
+    println!("{}", add('*', 2, 3));
     println!("Hello, world!");
 }
 
-fn add(a: u8, b: u8) -> u8 {
-    a + b
+fn add(op: char, a: u8, b: u8) -> u8 {
+    if op == '+' {
+        a + b
+    } else {
+        a * b
+    }
 }
 
